6850e0a9ca981384d8b3a970f7c348e25670931b
atifc1234 <58500471+atifc1234@users.noreply.github.com>
2020-01-13 21:14:37 -0700

    Add files via upload

--- EndScene.js
+++ EndScene.js
@@ -2,4 +2,7 @@ class EndScene extends Phaser.Scene {
     constructor() {
         super({ key: 'EndScene'})
     }
+    create() {
+        this.add.text(150, 50, "Congrats! You've reached the end of the \ngame. You may be wondering where is the human. \nDead. All the humans have died", {align: 'center', fontFamily: 'Georgia', fill: '#000000', fontSize: '15px'});
+    }
 }
