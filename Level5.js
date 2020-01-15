4e23bf84c4336e0ec85e60eb9488abe7bc13341a
atifc1234 <58500471+atifc1234@users.noreply.github.com>
2020-01-15 13:18:23 -0700

    Update Level5.js

--- Level5.js
+++ Level5.js
@@ -1,1 +1,50 @@
-
+class Level5 extends Phaser.Scene  {
+    constructor() {
+        super({ key: 'Level5'})
+    }
+    preload() {
+        this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/codey.png');
+        this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
+    }
+    createPlatforms(x, y, size) {
+        const platforms = this.physics.add.staticGroup();
+        platforms.create(x, y, 'platform').setScale(size).refreshBody(); 
+        this.physics.add.collider(gameState.player, platforms);
+    }
+    createJumpPads(x, y, size) {
+        const jumpPads = this.physics.add.staticGroup();
+        jumpPads.create(x, y, 'platform').setScale(size).setTint(0x000000).refreshBody();
+        this.physics.add.collider(gameState.player, jumpPads, function() {
+            gameState.player.setVelocityY(-500)
+        });
+    }
+    createTeleporters(x1, y1, size1, x2, y2, size2) {
+        const teleporter2 = this.physics.add.staticGroup();
+        teleporter2.create(x1, y1, 'platform').setScale(size1).setTint(0xfa67d8).refreshBody();
+        this.physics.add.collider(gameState.player, teleporter2);
+        const teleporter1 = this.physics.add.staticGroup();
+        teleporter1.create(x2, y2, 'platform').setScale(size2).setTint(0xfa67d8).refreshBody();
+        this.physics.add.collider(gameState.player, teleporter1, ()=> {
+          gameState.player.x = x1;
+          gameState.player.y = y1 - 20;
+        });
+    }
+    create() {
+        gameState.player = this.physics.add.sprite(10, 20, 'codey').setScale(.5);
+        const platforms = this.physics.add.staticGroup();
+        platforms.create(320, 580, 'platform').setScale(1.5).refreshBody();
+        gameState.player.setCollideWorldBounds(true);
+    }
+     update() {
+        const cursors = this.input.keyboard.createCursorKeys();
+        if (cursors.left.isDown) {
+            gameState.player.setVelocityX(-200);
+        } else if (cursors.right.isDown) {
+            gameState.player.setVelocityX(200);
+        } else {
+            gameState.player.setVelocityX(0);
+        };
+        if (cursors.up.isDown && gameState.player.body.touching.down) {
+            gameState.player.setVelocityY(-250)
+        };
+    }
